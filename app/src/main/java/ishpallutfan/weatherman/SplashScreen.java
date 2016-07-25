61629d952abd0a87bf1bdb09ba09e4de33c84241
Lutfan <lutfan@gmail.com>
2016-07-25 18:54:13 +0800

    The latest update. Splash still does not go to Main Activity

--- app/src/main/java/ishpallutfan/weatherman/SplashScreen.java
+++ app/src/main/java/ishpallutfan/weatherman/SplashScreen.java
@@ -6,37 +6,86 @@ package ishpallutfan.weatherman;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SplashScreen extends Activity {
 
-    TextView t;
+    String tentative = "test";
+    Thread timerThread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        final TextView t2 = (TextView) findViewById(R.id.textView2);
+        final TextView yT = (TextView) findViewById(R.id.yesText);
+        final TextView nT = (TextView) findViewById(R.id.noText);
 
         //This portion makes the fonts nice
         /*t =  (TextView) findViewById(R.id.header);
         Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "Fonts/Roboto-Thin.ttf");
         t.setTypeface(myCustomFont);*/
 
-        Thread timerThread = new Thread(){
+        timerThread = new Thread(){
             public void run(){
                 try{
                     sleep(3000);
+
                 }catch(InterruptedException e){
                     e.printStackTrace();
-                }finally{
-                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                    startActivity(intent);
+
+                } finally {
+
+                    runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //Toast.makeText(this,"hi", Toast.LENGTH_SHORT).show();
+                        t2.setTextColor(ColorStateList.valueOf(Color.parseColor("#E0F7FA")));
+                        yT.setTextColor(ColorStateList.valueOf(Color.parseColor("#E0F7FA")));
+                        nT.setTextColor(ColorStateList.valueOf(Color.parseColor("#E0F7FA")));
+                        //Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                        //startActivity(intent);
+                        }
+                    });
+
                 }
             }
         };
         timerThread.start();
+        //timerThread.interrupt();
+    }
+
+
+    public void onClick(View v){
+        Toast.makeText(this,"hi", Toast.LENGTH_SHORT).show();
+        switch (v.getId()) {
+            case R.id.yesButton:
+                boolean isRaining = true;
+
+                // to pass the age to the next activity
+                Intent intentBundle1 = new Intent(this, MainActivity.class);
+                Bundle bundle1 = new Bundle();
+                bundle1.putBoolean("isRaining", isRaining);
+                intentBundle1.putExtras(bundle1);
+                startActivity(intentBundle1);
+
+
+            case R.id.noButton:
+                isRaining = true;
+
+                // to pass the age to the next activity
+                Intent intentBundle2 = new Intent(this, MainActivity.class);
+                Bundle bundle2 = new Bundle();
+                bundle2.putBoolean("isRaining", isRaining);
+                intentBundle2.putExtras(bundle2);
+                startActivity(intentBundle2);
+        }
     }
 
     @Override
