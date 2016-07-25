ebfb3a56e217e13afe03ca6dcf173be31c6d7a59
ishpal26 <ishpalsinghgill@live.co.uk>
2016-07-26 00:17:59 +0800

    Added location finder class

---
+++ app/src/main/java/ishpallutfan/weatherman/LocationFinder.java
@@ -0,0 +1,11 @@
+package ishpallutfan.weatherman;
+
+import android.location.Location;
+
+/**
+ * Created by Ishpal on 25/7/2016.
+ */
+public class LocationFinder {
+    public LocationFinder(){
+    }
+}
